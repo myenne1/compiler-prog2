fa4968db60e978de314184b9979bbd6316635c81
KadenC707 <kcase14@lsu.edu>
2025-10-15 23:13:57 -0500

    Prettying the code

--- Absyn/FieldExpList.java
+++ Absyn/FieldExpList.java
@@ -4,7 +4,6 @@ public class FieldExpList extends Absyn {
    public Symbol name;
    public Exp init;
    public FieldExpList tail;
-   public FieldExpList(int p, Symbol n, Exp i, FieldExpList t) {pos=p; 
-	name=n; init=i; tail=t;
-   }
+   public FieldExpList(int p, Symbol n, Exp i, FieldExpList t) 
+	{pos=p; name=n; init=i; tail=t;}
 }
